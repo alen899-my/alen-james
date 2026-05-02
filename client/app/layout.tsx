c359148d2f6b8f7c95235ab65f9e703b5d860c9c
alen899-my <alenjames899@gmail.com>
2026-05-02 23:13:41 +0530

    feat: initialize root layout with metadata and Umami analytics script

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="0848ea46-9878-4c9b-96d4-ed6ae8cbac09"></script>
+      </head>
       <body style={{ overflowX: 'hidden' }}>
         {children}
       </body>
